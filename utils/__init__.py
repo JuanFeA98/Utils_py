f4ea94d87a55491f529abe347ceef599ddc53086
JuanFe <jmartinezbernal02@gmail.com>
2023-02-20 21:51:51 -0500

    Configuración betha

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'utils'
+__version__ = '0.0.1'
+__author__ = 'JuanFe98'
+__email__ = 'jmartinezbernal02@gmail.com'
+__description__ = 'Modulo de utilidades de Python'
+__url__ = 'https://github.com/JuanFeA98/Utils_py'
